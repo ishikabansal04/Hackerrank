c80a1a3c3646583cb854d1987aece832b349b459
Ishika Bansal <49348251+ishikabansal04@users.noreply.github.com>
2021-06-11 23:05:32 +0530

    Create InheritedCode.cpp

---
+++ C++/Classes/InheritedCode.cpp
@@ -0,0 +1,9 @@
+
+
+/* Define the exception here */
+class BadLengthException: public exception {
+    int N;
+    public:
+    BadLengthException(int N){this->N = N;}
+    int what(){return N;}
+};
